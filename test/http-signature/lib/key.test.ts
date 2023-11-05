eae9acff329ddff97ce845d0a438a214f4841893
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-11-05 15:45:28 +0800

    update http-signature key test

--- test/http-signature/lib/key.test.ts
+++ test/http-signature/lib/key.test.ts
@@ -11,7 +11,7 @@ import {
 
 // fail in Node.js
 Deno.test({
-  name: 'key',
+  name: 'generate / export / import',
   // @ts-ignore only used to detect node.js
   ignore: globalThis?.process?.release?.name === 'node',
   fn: async () => {
@@ -35,7 +35,15 @@ Deno.test({
 Deno.test('importPublicKey / fetchPublicKey', async () => {
   /** from {@link https://github.com/LemmyNet/lemmy/blob/main/crates/apub/assets/mastodon/objects/person.json} */
   const mastodonPublicKeyImport = await importPublicKey(
-    '-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAtBdE55VmV9gTrhJmRF1K\neX7xTRo17JGQ7d1/KJWsQ1zH62GGeG/E+BG3h/BRtfgI7Z9jwfNEyx8g/Ue8rSeZ\n3M7yc09/Z90uwGVY24hxwAJyzWIN2cv5ayhdtk268byT6NX98a9PQcHlx5i6Bhef\nMlpY73I5gxYlofvwJTHq/VupXVw9K76KId2AgR2z8tLiXPc8TED56HulDWdMlWn3\n9B4mWNYmzMBF7lOl58Ws6bFsiv8GnI3uEywzUGhXqz4242FGveHdAGBaCpUYrm8W\nmT8PArqv3B4fCD1ghakSmxRr3y9clwhkC+kB/aoT6z313uZYbQuvZF1bfbh6EZWm\nIQIDAQAB\n-----END PUBLIC KEY-----\n',
+    `-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAtBdE55VmV9gTrhJmRF1K
+eX7xTRo17JGQ7d1/KJWsQ1zH62GGeG/E+BG3h/BRtfgI7Z9jwfNEyx8g/Ue8rSeZ
+3M7yc09/Z90uwGVY24hxwAJyzWIN2cv5ayhdtk268byT6NX98a9PQcHlx5i6Bhef
+MlpY73I5gxYlofvwJTHq/VupXVw9K76KId2AgR2z8tLiXPc8TED56HulDWdMlWn3
+9B4mWNYmzMBF7lOl58Ws6bFsiv8GnI3uEywzUGhXqz4242FGveHdAGBaCpUYrm8W
+mT8PArqv3B4fCD1ghakSmxRr3y9clwhkC+kB/aoT6z313uZYbQuvZF1bfbh6EZWm
+IQIDAQAB
+-----END PUBLIC KEY-----`,
   )
   const mastodonPublicKeyFetch = await fetchPublicKey(
     'https://raw.githubusercontent.com/LemmyNet/lemmy/main/crates/apub/assets/mastodon/objects/person.json#main-key',
@@ -43,7 +51,15 @@ Deno.test('importPublicKey / fetchPublicKey', async () => {
 
   /** from {@link https://github.com/LemmyNet/lemmy/blob/main/crates/apub/assets/pleroma/objects/person.json} */
   const pleromaPublicKeyImport = await importPublicKey(
-    '-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAsWOgdjSMc010qvxC3njI\nXJlFWMJ5gJ8QXCW/PajYdsHPM6d+jxBNJ6zp9/tIRa2m7bWHTSkuHQ7QthOpt6vu\n+dAWpKRLS607SPLItn/qUcyXvgN+H8shfyhMxvkVs9jXdtlBsLUVE7UNpN0dxzqe\nI79QWbf7o4amgaIWGRYB+OYMnIxKt+GzIkivZdSVSYjfxNnBYkMCeUxm5EpPIxKS\nP5bBHAVRRambD5NUmyKILuC60/rYuc/C+vmgpY2HCWFS2q6o34dPr9enwL6t4b3m\nS1t/EJHk9rGaaDqSGkDEfyQI83/7SDebWKuETMKKFLZi1vMgQIFuOYCIhN6bIiZm\npQIDAQAB\n-----END PUBLIC KEY-----\n\n',
+    `-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAsWOgdjSMc010qvxC3njI
+XJlFWMJ5gJ8QXCW/PajYdsHPM6d+jxBNJ6zp9/tIRa2m7bWHTSkuHQ7QthOpt6vu
++dAWpKRLS607SPLItn/qUcyXvgN+H8shfyhMxvkVs9jXdtlBsLUVE7UNpN0dxzqe
+I79QWbf7o4amgaIWGRYB+OYMnIxKt+GzIkivZdSVSYjfxNnBYkMCeUxm5EpPIxKS
+P5bBHAVRRambD5NUmyKILuC60/rYuc/C+vmgpY2HCWFS2q6o34dPr9enwL6t4b3m
+S1t/EJHk9rGaaDqSGkDEfyQI83/7SDebWKuETMKKFLZi1vMgQIFuOYCIhN6bIiZm
+pQIDAQAB
+-----END PUBLIC KEY-----`,
   )
   const pleromaPublicKeyFetch = await fetchPublicKey(
     'https://github.com/LemmyNet/lemmy/raw/main/crates/apub/assets/pleroma/objects/person.json',
