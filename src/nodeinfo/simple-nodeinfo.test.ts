cbde811d2bd84e76763545ac36f5ca5cd6a3642e
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-11-08 23:16:16 +0800

    chore(nodeinfo): more test

---
+++ src/nodeinfo/simple-nodeinfo.test.ts
@@ -0,0 +1,67 @@
+import { assertEquals } from 'std/assert/mod.ts'
+import { Hono } from 'hono/mod.ts'
+import { type NodeInfo, simpleNodeInfo } from './mod.ts'
+
+const app = new Hono()
+
+app.get('/nodeinfo/*', ({ req }) =>
+  simpleNodeInfo(req.raw, {
+    software: {
+      name: 'fedikit',
+      version: '0.0.1',
+      repository: 'https://github.com/fedikit/fedikit.git',
+      homepage: 'https://github.com/fedikit/fedikit#readme',
+    },
+  }))
+
+const expectedNodeInfo21 = {
+  version: '2.1',
+  software: {
+    name: 'fedikit',
+    version: '0.0.1',
+    repository: 'https://github.com/fedikit/fedikit.git',
+    homepage: 'https://github.com/fedikit/fedikit#readme',
+  },
+  protocols: ['activitypub'],
+  services: {
+    inbound: [],
+    outbound: [],
+  },
+  openRegistrations: false,
+  usage: {
+    users: {},
+  },
+  metadata: {},
+} satisfies NodeInfo<'2.1'>
+
+const expectedNodeInfo20 = {
+  version: '2.0',
+  software: {
+    name: 'fedikit',
+    version: '0.0.1',
+  },
+  protocols: ['activitypub'],
+  services: {
+    inbound: [],
+    outbound: [],
+  },
+  openRegistrations: false,
+  usage: {
+    users: {},
+  },
+  metadata: {},
+} satisfies NodeInfo<'2.0'>
+
+Deno.test('GET /nodeinfo/2.1', async () => {
+  const res = await app.request('/nodeinfo/2.1')
+
+  assertEquals(res.status, 200)
+  assertEquals(await res.json(), expectedNodeInfo21)
+})
+
+Deno.test('GET /nodeinfo/2.0', async () => {
+  const res = await app.request('/nodeinfo/2.0')
+
+  assertEquals(res.status, 200)
+  assertEquals(await res.json(), expectedNodeInfo20)
+})
---
+++ src/nodeinfo/simple-well-known-nodeinfo.test.ts
@@ -0,0 +1,55 @@
+import { assertEquals } from 'std/assert/mod.ts'
+import { Hono } from 'hono/mod.ts'
+import { simpleWellKnownNodeInfo } from './mod.ts'
+
+Deno.test('GET /.well-known/nodeinfo (localhost)', async () => {
+  const app = new Hono()
+  app.get(
+    '/.well-known/nodeinfo',
+    ({ req }) => simpleWellKnownNodeInfo(new URL(req.raw.url).origin),
+  )
+
+  const expectedWellKnownNodeInfo = {
+    links: [
+      {
+        rel: 'http://nodeinfo.diaspora.software/ns/schema/2.0',
+        href: 'http://localhost/nodeinfo/2.0',
+      },
+      {
+        rel: 'http://nodeinfo.diaspora.software/ns/schema/2.1',
+        href: 'http://localhost/nodeinfo/2.1',
+      },
+    ],
+  }
+
+  const res = await app.request('/.well-known/nodeinfo')
+
+  assertEquals(res.status, 200)
+  assertEquals(await res.json(), expectedWellKnownNodeInfo)
+})
+
+Deno.test('GET /.well-known/nodeinfo (example.com)', async () => {
+  const app = new Hono()
+  app.get(
+    '/.well-known/nodeinfo',
+    () => simpleWellKnownNodeInfo('https://example.com'),
+  )
+
+  const expectedWellKnownNodeInfo = {
+    links: [
+      {
+        rel: 'http://nodeinfo.diaspora.software/ns/schema/2.0',
+        href: 'https://example.com/nodeinfo/2.0',
+      },
+      {
+        rel: 'http://nodeinfo.diaspora.software/ns/schema/2.1',
+        href: 'https://example.com/nodeinfo/2.1',
+      },
+    ],
+  }
+
+  const res = await app.request('/.well-known/nodeinfo')
+
+  assertEquals(res.status, 200)
+  assertEquals(await res.json(), expectedWellKnownNodeInfo)
+})
