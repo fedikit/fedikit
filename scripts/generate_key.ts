25d078fb60c4ddaa0fe06ee234ac9deaa486b458
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-11-15 14:06:18 +0800

    chore: fmt code

--- scripts/generate_key.ts
+++ scripts/generate_key.ts
@@ -1,11 +1,15 @@
 #!/usr/bin/env -S deno run
 
-import { generateKey, exportPrivateKey, exportPublicKey } from '../src/http-signature/lib/key.ts'
+import {
+  exportPrivateKey,
+  exportPublicKey,
+  generateKey,
+} from '../src/http-signature/lib/key.ts'
 
 const keyPair = await generateKey({
-  modulusLength: [2048, 3072, 4096].some(length => +Deno.args[0] === length)
+  modulusLength: [2048, 3072, 4096].some((length) => +Deno.args[0] === length)
     ? +Deno.args[0] as 2048 | 3072 | 4096
-    : 4096
+    : 4096,
 })
 
 console.log(`%c${await exportPrivateKey(keyPair)}`, 'color: red')
