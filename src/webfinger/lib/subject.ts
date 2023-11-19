4dd9ff4124c412a4b34687fe16b2994d7d1835f5
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-11-19 19:15:35 +0800

    feat(webfinger): fetch subject util

--- src/webfinger/lib/subject.ts
+++ src/webfinger/lib/subject.ts
@@ -1,7 +1,20 @@
-export const extractSubject = (subject: string) => {
+import type { Webfinger } from './types.ts'
+
+export const parseSubject = (subject: string) => {
   const [user, host] = subject
     .slice(5) // remove `acct:`
     .split('@') // split
 
   return { user, host }
+}
+
+export const fetchSubject = async (subject: string) => {
+  const { host } = parseSubject(subject)
+
+  return await fetch(
+    `https://${host}/.well-known/webfinger?resource=${subject}`,
+    {
+      headers: { 'Accept': 'application/jrd+json' },
+    },
+  ).then((res) => res.json() as unknown as Webfinger)
 }
